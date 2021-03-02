1240cb69d512d5714043b048ee2b074ce88fbe41
joy <joy.osive@gmail.com>
2021-03-03 05:11:48 +0530

    aded mockeServer mongo + backend express

---
+++ spring-mongo-mockServer/src/main/java/com/example/OracleMongo/OracleMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OracleMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleMongoApplication.class, args);
+	}
+
+}
